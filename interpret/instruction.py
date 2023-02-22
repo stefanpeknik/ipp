a92b310f6ae98595239e1d280eefb28f4e576441
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-02-22 15:46:07 +0100

    add typing checks

--- interpret/instruction.py
+++ interpret/instruction.py
@@ -1,6 +1,11 @@
 import sys
 import errorCodes as err
+from variable import Variable
+from frameStack import FrameStack
+from callStack import CallStack
+from dataStack import DataStack
 from frame import Frame
+from typing import List, Dict, Tuple
 
 
 class Instruction:
@@ -43,32 +48,32 @@ class Instruction:
         }
 
     @property
-    def opcode(self):
+    def opcode(self) -> str:
         return self._opcode
 
     @property
-    def args(self):
+    def args(self) -> Tuple[Variable]:
         return self._args
 
-    def execute(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def execute(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return self._op[self._opcode](GF, TF, LF_stack, read_from)
 
-    def isConstant(self, arg):
+    def isConstant(self, arg) -> bool:
         return arg.type == "string" or arg.type == "int" or arg.type == "bool" or arg.type == "nil"
 
-    def isVar(self, arg):
+    def isVar(self, arg) -> bool:
         return arg.type == "var"
 
-    def isLabel(self, arg):
+    def isLabel(self, arg) -> bool:
         return arg.type == "label"
 
-    def isType(self, arg):
+    def isType(self, arg) -> bool:
         return arg.type == "type"
 
-    def getVarNameAndScope(self, arg):
+    def getVarNameAndScope(self, arg) -> Tuple[str]:
         return arg.value.split("@")
 
-    def findVar(self, arg, GF, TF, LF_stack):
+    def findVar(self, arg, GF: Frame, TF: Frame, LF_stack: FrameStack) -> Variable:
         scope, name = arg.value.split("@")
         if scope == "GF":
             return self.getVarFromGF(name, GF)
@@ -77,37 +82,37 @@ class Instruction:
         elif scope == "LF":
             return self.getVarFromLF(name, LF_stack)
 
-    def getVarFromGF(self, name, GF):
+    def getVarFromGF(self, name, GF) -> Variable:
         return GF.getVar(name)
 
-    def getVarFromTF(self, name, TF):
+    def getVarFromTF(self, name, TF) -> Variable:
         if TF.defined:
             return TF.getVar(name)
         else:
             sys.exit(err.ERR_FRAME_NOT_FOUND)
 
-    def getVarFromLF(self, name, LF_stack):
+    def getVarFromLF(self, name, LF_stack) -> Variable:
         if LF_stack.is_empty() or not LF_stack.top().defined:
             sys.exit(err.ERR_FRAME_NOT_FOUND)
         else:
             return LF_stack.top().getVar(name)
 
-    def setVarInGF(self, name, GF, type, value):
+    def setVarInGF(self, name, GF, type, value) -> None:
         GF.getVar(name).set(type, value)
 
-    def setVarInTF(self, name, TF, type, value):
+    def setVarInTF(self, name, TF, type, value) -> None:
         if TF.defined:
             TF.getVar(name).set(type, value)
         else:
             sys.exit(err.ERR_FRAME_NOT_FOUND)
 
-    def setVarInLF(self, name, LF_stack, type, value):
+    def setVarInLF(self, name, LF_stack, type, value) -> None:
         if LF_stack.is_empty() or not LF_stack.top().defined:
             sys.exit(err.ERR_FRAME_NOT_FOUND)
         else:
             LF_stack.top().getVar(name).set(type, value)
 
-    def setVar(self, arg, GF, TF, LF_stack, type, value):
+    def setVar(self, arg, GF, TF, LF_stack, type, value) -> Tuple:
         scope, name = self.getVarNameAndScope(
             arg)
         if scope == "GF":
@@ -120,7 +125,7 @@ class Instruction:
             sys.exit(err.ERR_INTERNAL)
         return GF, TF, LF_stack
 
-    def defVar(self, arg, GF, TF, LF_stack):
+    def defVar(self, arg, GF, TF, LF_stack) -> Tuple:
         scope, name = self.getVarNameAndScope(arg)
         if scope == "GF":
             GF.addVar(name)
@@ -130,7 +135,7 @@ class Instruction:
             LF_stack.top().addVar(name)
         return GF, TF, LF_stack
 
-    def _move(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _move(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         if self.isVar(self.args[1]):
             var = self.findVar(self.args[1], GF, TF, LF_stack)
             if not var.inited:
@@ -145,11 +150,11 @@ class Instruction:
 
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _createframe(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _createframe(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int][str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         TF = Frame()
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _pushframe(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _pushframe(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         if not TF.defined:
             sys.exit(err.ERR_FRAME_NOT_FOUND)
         LF_stack.push(TF)
@@ -157,98 +162,94 @@ class Instruction:
         TF.defined = False
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _popframe(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _popframe(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         TF = LF_stack.pop()
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _defvar(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
-        try:
-            self.findVar(self.args[0], GF, TF, LF_stack)
-            sys.exit(err.ERR_SEMANTIC_ERROR)
-        except:
-            GF, TF, LF_stack = self.defVar(
-                self.args[0], GF, TF, LF_stack)
+    def _defvar(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
+        # TODO
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _call(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _call(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         call_stack.push(ins_num)
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _return(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _return(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         ins_num = call_stack.pop()
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _pushs(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _pushs(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
+        # TODO
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _pops(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _pops(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _add(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _add(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _sub(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _sub(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _mul(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _mul(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _idiv(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _idiv(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _lt(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _lt(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _gt(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _gt(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _eq(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _eq(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _and(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _and(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _or(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _or(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _not(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _not(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _int2char(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _int2char(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _stri2int(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _stri2int(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _read(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _read(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _write(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _write(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _concat(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _concat(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _getchar(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _getchar(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _setchar(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _setchar(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _type(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _type(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _label(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _label(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _jump(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _jump(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _jumpifeq(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _jumpifeq(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _jumpifneq(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _jumpifneq(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
 
-    def _exit(self, ins_num, GF, TF, LF_stack, labels, call_stack, data_stack, read_from):
+    def _exit(self, ins_num: int, GF: Frame, TF: Frame, LF_stack: FrameStack, labels: Dict[str, int], call_stack: CallStack, data_stack: DataStack, read_from: str) -> Tuple:
         return ins_num, GF, TF, LF_stack, labels, call_stack, data_stack
