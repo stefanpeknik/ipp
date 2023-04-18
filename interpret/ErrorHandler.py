dbd07124e331f6b7a9e8f39f15e8e755c36cffa3
Stefan Peknik <xpekni01@stud.fit.vutbr.cz>
2023-04-18 20:07:25 +0200

    rename arg

--- interpret/ErrorHandler.py
+++ interpret/ErrorHandler.py
@@ -3,6 +3,6 @@ import sys
 
 class ErrorHandler:
     @staticmethod
-    def err_exit(exit_code: int, e: Exception):
-        print(e, file=sys.stderr)
+    def err_exit(exit_code: int, exception: Exception):
+        print(exception, file=sys.stderr)
         sys.exit(exit_code)
